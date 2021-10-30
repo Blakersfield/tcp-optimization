84360b4fc6c0f158c1147647323b2b744bad5ed4
Blake <blake.pinette@gmail.com>
2021-10-29 19:29:11 -0700

    app.py updates were missing from commit

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import send_from_directory
 
 app = Flask(__name__)
+app.config['STATIC_FOLDER'] = app.root_path + '\static\'
+
 
 @app.route("/")
 def send_data():
-    return "text out for harambe"
+    return app.config['STATIC_FOLDER'] #"text out for harambe"
+
+@app.route("/test")
+def send_file():
+    return send_from_directory(app.config['STATIC_FOLDER'], 'static/harambe.jpg', as_attachment=True)
