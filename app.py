1edbd7441d981f1025e328183cd07b3aeec8c97f
blake <blake.pinette@gmail.com>
2021-11-02 20:08:08 -0700

    incorporate methods into app.py

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask
 from flask import send_from_directory
 from time import sleep
+from synchronization import *
 
 app = Flask(__name__)
 app.config['STATIC_FOLDER'] = app.root_path + '/static/'
-
+setting_startup()
 
 @app.route("/")
 def send_data():
     path = "<a href = \"/continuous\">test</a>"
     return path
 
+
 @app.route("/test")
 def send_file():
     return send_from_directory(app.config['STATIC_FOLDER'], 'harambe.jpg', as_attachment=True)
 
+
+@app.route("/sync-transfer")
+def sync_transfer():
+    connection_wait()
+    return send_from_directory(app.config['STATIC_FOLDER'], 'harambe.jpg', as_attachment=True)
+
+
 @app.route("/continuous")
 def cont_transf():
+    #add to
     sleep(300)
     return send_from_directory(app.config['STATIC_FOLDER'], 'harambe.jpg', as_attachment=True)
---
+++ synchronization.py
@@ -0,0 +1,57 @@
+import time
+
+
+def connection_wait():
+    """wait until connection count matches target count"""
+    settings_dict = read_settings()
+    settings_dict.update({"connection_count": str(int(settings_dict["connection_count"]) + 1)})
+    cont = True
+    while cont:
+        settings_dict = read_settings()
+        if get_conn_count(settings_dict) == get_targ_conn(settings_dict):
+            cont = False
+        else:
+            time.sleep(time_sync())
+
+
+def time_sync(window=5):
+    """return the number of seconds until the next multiple of 5"""
+    current_time = int(time.time())
+    difference = current_time % window
+    return window - difference
+
+
+def setting_startup():
+    """set connections to 0 while perservering other settings"""
+    settings_dict = read_settings()
+    settings_dict.update({"connection_count": "0"})
+    write_settings(settings_dict)
+
+
+def read_settings():
+    """return dict with settings from file"""
+    file = open("test_settings.txt", "r")
+    settings = file.readlines()
+    output = {}
+    for row in settings:
+        parsed_row = row.split("=")
+        output.update({parsed_row[0]: parsed_row[1]})
+    file.close()
+    return output
+
+
+def write_settings(settings_dict):
+    """write settings dict to file"""
+    file = open("test_settings.txt", "w")
+    keys = list(settings_dict.keys())
+    for key in keys:
+        file.write(key + "=" + settings_dict[key] + "\n")
+    file.close()
+
+
+def get_conn_count(settings_dict):
+    return settings_dict["connection_count"]
+
+
+def get_targ_conn(settings_dict):
+    return settings_dict["target_connection"]
