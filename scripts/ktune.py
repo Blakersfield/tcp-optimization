d35ea7deebdcade9d9f11c5db9517fa6a076a073
Blake <blake.pinette@gmail.com>
2021-11-09 17:45:05 -0800

    add cpu interrupt methods

--- scripts/ktune.py
+++ scripts/ktune.py
@@ -5,30 +5,48 @@ def cc(algo="reno", readback=True):
     terminal = "sysctl -w net.ipv4.tcp_congestion_control=" + algo
     proc=subprocess.Popen(terminal, shell=True,
                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if readback:
-        print(proc.stdout.readline().strip())
+    readback_term(proc, readback)
 
 
 def rmem(minv=4096, maxv=6291456, startv=131072, readback=True):
     terminal = "sysctl -w net.ipv4.tcp_rmem=\"" + str(minv) + " " + str(startv) + " " + str(maxv)+"\""
     proc = subprocess.Popen(terminal, shell=True,
                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if readback:
-        print(proc.stdout.readline().strip())
+    readback_term(proc, readback)
+
 
 def wmem(minv=4096, maxv=4194304, startv=16384, readback=True):
     terminal = "sysctl -w net.ipv4.tcp_wmem=\"" + str(minv) + " " + str(startv) + " " + str(maxv)+"\""
     proc = subprocess.Popen(terminal, shell=True,
                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if readback:
-        print(proc.stdout.readline().strip())
+    readback_term(proc, readback)
 
 
-def win_scale(binary):
+def int_queue(size, readback=True, device="eth0"):
+    terminal = "ifconfig " + device + " txqueuelen " + size
+    proc = subprocess.Popen(terminal, shell=True,
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    readback_term(proc, readback)
+
+
+def cpu_int(size, readback=True, device="eth0"):
+    terminal = "ethtool -k " + device +  "tx on"
+    proc = subprocess.Popen(terminal, shell=True,
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    readback_term(proc, readback)
+
+
+def win_scale(binary, readback=True):
     param = None
     if binary:
-        param=1
+        param = 1
     else:
-        param=0
+        param = 0
     terminal = "sysctl -w net.ipv4.tcp_window_scaling=" + str(param)
-    subprocess.Popen(terminal, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen(terminal, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    readback_term(proc, readback)
+
+
+def readback_term(proc, binary):
+    if binary:
+        print(proc.stdout.readline().strip())
