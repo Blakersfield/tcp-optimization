5317d7201879eec49fdc179f7010d82aef231706
blakersfield <blake.pinette@gmail.com>
2021-11-10 18:14:13 -0800

    change target to use parameter

--- client/downloader.py
+++ client/downloader.py
@@ -1,9 +1,10 @@
 from urllib import request 
 import shutil
 
-def launch():
+
+def launch(target):
     print('Launching client app...')
-    url = 'http://127.0.0.1:5000/test'
+    url = 'http://' + target + '/test'
     file_name = 'harambe.jpg'
     with request.urlopen(url) as response, open(file_name, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
