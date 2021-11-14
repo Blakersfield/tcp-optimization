e908d860d056f664027eab3fafdd3118602fdad5
Giovanni Flores <gdr943@gmail.com>
2021-11-14 00:37:48 +0000

    added port number

--- client/downloader.py
+++ client/downloader.py
@@ -4,7 +4,7 @@ import shutil
 
 def launch(target):
     print('Launching client app...')
-    url = 'http://' + target + '/sync-transfer'
+    url = 'http://' + target + ':5000/sync-transfer'
     file_name = 'arch.tar'
     with request.urlopen(url) as response, open(file_name, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
